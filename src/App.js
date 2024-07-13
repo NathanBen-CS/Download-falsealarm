3e63084a7092e9b7c3fb9d1e6e16327f328e3c87
nathan <nathanbenayguev9@gmail.com>
2024-07-13 03:00:46 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const ZIP_FILE_URL = 'http://localhost:3000/FalseAlarm.zip';
+
 function App() {
+ 
+  const download = () => {
+    const aTag = document.createElement('a');
+    aTag.href = ZIP_FILE_URL;
+    aTag.setAttribute('download', 'FalseAlarm.zip');
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Download False Alarm</h1>
+      <b>
+        False Alarm is a traffic racing game designed to be played on a gamepad/controller. The aim of the game is to dodge cars on the road while maintaining a certain speed limit
+        that is constantly rising in increments. Failure to keep up or crashing into other cars results in the player exploding. There are multiple styles of cars on the road that all
+        have different behaviors, and it is your job to act accordingly. The game also tracks various achievements and scores during each run and saves them to a file.
+        To support False Alarm, please leave a rating and comment for feedback. This is a game programmed in XNA/Monogame using C# to demonstrate OOP, polymorphism,
+        custom data structures such as linked lists and queues, and more. Thanks for checking out False Alarm!
+      </b>
+
+      <button onClick={download}>Download FalseAlarm</button>
     </div>
   );
 }
